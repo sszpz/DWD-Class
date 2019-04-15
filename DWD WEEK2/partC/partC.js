bb5303f8a4b6eea846f96555a8105c3a24908c4f
Yuanyuan Wang <wangyuan_ad@163.com>
2019-04-15 14:49:20 -0400

    Update partC.js

--- DWD WEEK2/partC/partC.js
+++ DWD WEEK2/partC/partC.js
@@ -1,1 +1,21 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+app.get('/index.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
+})
+
+app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   url_name = req.query.url_name;
+   res.redirect(url_name)
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
 
